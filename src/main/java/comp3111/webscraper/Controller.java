e4de60eb8997fa0343abe24b1314537f13460e2d
Unknown <esmond9981127@gmail.com>
2018-10-11 13:37:13 +0800

    Change back the labelCount :man_shrugging:

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -69,8 +69,6 @@ public class Controller {
     		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
     	}
     	textAreaConsole.setText(output);
-
-    	labelCount.setText("Hi");
     }
     
     /**
